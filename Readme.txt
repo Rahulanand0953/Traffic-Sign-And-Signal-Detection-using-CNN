c77492ba63278c9c6bdb7632c1648281b6a53e40
Rahulanand0953 <50734581+Rahulanand0953@users.noreply.github.com>
2021-09-15 21:48:46 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,16 @@
+Steps To execute:
+
+1. For Running the Image Classsifier:
+
+	1.1 Open command prompt from the directory where the source code is saved
+	1.2  Run the python file using the command: "python model.py”
+	1.3 After running the above command “ traffic_classifier.h5 “ will be generated.
+	1.4 Run the python file using the command: "python ImageClassifier.py"
+	1.5 Use upload button to upload the imge which is needed to be classified
+	1.6 Click on Classify button to finally predict the label for the image
+
+2. For Running the Image Classsifier:
+
+	2.1 Open command prompt from the directory where the source code is saved
+	2.2 Run the python file using the command: "python VideoClassifier.py"
+
---
+++ model.py
@@ -0,0 +1,64 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from PIL import Image
+import os
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from sklearn.metrics import accuracy_score
+data = []
+labels = []
+classes = 46
+cur_path = os.getcwd()
+for i in range(classes):
+    path = os.path.join(cur_path,'train',str(i))
+    images = os.listdir(path)
+    for a in images:
+        try:
+            image = Image.open(path + '\\'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            data.append(image)
+            labels.append(i)
+        except:
+            print("Error loading image")
+data = np.array(data)
+labels = np.array(labels)
+print(data.shape, labels.shape)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+y_train = to_categorical(y_train, 46)
+y_test = to_categorical(y_test, 46)
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(46, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+epochs = 15
+history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+y_test = pd.read_csv('Test.csv')
+labels = y_test["ClassId"].values
+imgs = y_test["Path"].values
+data=[]
+for img in imgs:
+    image = Image.open(img)
+    image = image.resize((30,30))
+    data.append(np.array(image))
+X_test=np.array(data)
+pred = model.predict_classes(X_test)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels, pred))
+model.save('traffic_classifier.h5')
